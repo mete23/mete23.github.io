070e8804332fbaf7a88287c16309e51aea7a5f56
mete23 <mete.yayla@web.de>
2025-01-06 19:31:41 +0100

    added tilt

--- tilt.js
+++ tilt.js
@@ -3,6 +3,16 @@ function init() {
     window.addEventListener("deviceorientation", handleOrientation, true);
 
 
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', (event) => {
+            const acceleration = event.accelerationIncludingGravity;
+            document.getElementById('acceleration').innerText =
+                `Acceleration: x = ${acceleration.x.toFixed(2)}, y = ${acceleration.y.toFixed(2)}, z = ${acceleration.z.toFixed(2)}`;
+        });
+    } else {
+        document.getElementById('acceleration').innerText = 'Accelerometer not supported on this device.';
+    }
+
     function handleOrientation(orientation) {
         console.log(orientation);
         console.log(orientation.alpha);
