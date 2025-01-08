54378faed59a330a82c13b7bed1dbeb45b310444
mete23 <mete.yayla@web.de>
2025-01-08 17:17:10 +0100

    added rotation

--- tilt.js
+++ tilt.js
@@ -22,10 +22,26 @@ function initGyro() {
             const acceleration = event.accelerationIncludingGravity;
             document.getElementById('acceleration').innerText =
                 `Acceleration: x = ${acceleration.x.toFixed(2)}, y = ${acceleration.y.toFixed(2)}, z = ${acceleration.z.toFixed(2)}`;
+            
+            
         });
     } else {
         document.getElementById('acceleration').innerText = 'Accelerometer not supported on this device.';
     }
+}
+
+
+
+function rotateByGyro(value) {
+    var deg = 90 * (value / 10);
+    var div = document.querySelector('#webcam'),
+        deg = rotated ? 0 : 66;
+
+    div.style.webkitTransform = 'rotate('+deg+'deg)'; 
+    div.style.mozTransform    = 'rotate('+deg+'deg)'; 
+    div.style.msTransform     = 'rotate('+deg+'deg)'; 
+    div.style.oTransform      = 'rotate('+deg+'deg)'; 
+    div.style.transform       = 'rotate('+deg+'deg)'; 
 }
 
 window.addEventListener("load", initGyro, false);
