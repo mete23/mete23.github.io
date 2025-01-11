e6672ab2be786cfcbf533464f371eb385f87a76d
mete23 <mete.yayla@web.de>
2025-01-11 19:48:31 +0100

    fixed camera

--- camera.js
+++ camera.js
@@ -1,15 +1,14 @@
-const photo_width = window.width; //for scaling the photo
-var photo_height = 0; //will be computed
-var streaming = false;
+const photo_width = 640; // Set a default width for the photo
+let photo_height = 0; // Will be computed
+let streaming = false;
 
 let canvas = null;
 let capturedPhoto = null;
 let video = null;
-
 let downloadLink = null;
 
 function webcam(btn) {
-    if (btn == 'pause') {
+    if (btn === 'pause') {
         video.pause();
     } else {
         video.play();
@@ -17,83 +16,69 @@ function webcam(btn) {
 }
 
 function init() {
-
     video = document.querySelector('#webcam');
-    var captureButton = document.querySelector('#capture_button')
-    canvas = document.querySelector('#photo_canvas')
-    capturedPhoto = document.querySelector('#captured_photo')
+    const captureButton = document.querySelector('#capture_button');
+    canvas = document.querySelector('#photo_canvas');
+    capturedPhoto = document.querySelector('#captured_photo');
     downloadLink = document.querySelector('#download_link');
 
-
-    //starting the video stream:
+    // Starting the video stream
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
             .then(function (stream) {
                 video.srcObject = stream;
+                video.play();
             })
             .catch(function (e) {
                 console.log(e);
             });
     }
 
-    //adding
-    video.addEventListener(
-        "canplay",
-        (event) => {
-            if (!streaming) {
-                photo_height = (video.videoHeight / video.videoWidth) * photo_width;
+    video.addEventListener('canplay', (event) => {
+        if (!streaming) {
+            photo_height = (video.videoHeight / video.videoWidth) * photo_width;
 
-                //make assumption if height cannot be read
-                // if (isNan(photo_height)) {
-                //     height = width / (4/3);
-                // }
-
-                video.setAttribute("width", photo_width);
-                video.setAttribute("height", photo_height)
-                canvas.setAttribute("height", photo_height)
-                canvas.setAttribute("width", photo_width)
-                streaming = true
+            // Make assumption if height cannot be read
+            if (isNaN(photo_height)) {
+                photo_height = photo_width / (4 / 3);
             }
-        }, false,
-    );
-    
-    function clearCanvas() {
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#AAA"
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    
-        const data = canvas.toDataURL("image/png");
-    
-    }
 
-    function takePicture() {
-        const context = canvas.getContext("2d");
-        if (photo_width && photo_height) {
-            canvas.width = photo_width;
-            canvas.height = photo_height;
-            context.drawImage(video, 0, 0, photo_width, photo_height);
-
-            const data = canvas.toDataURL("image/png");
-            capturedPhoto.setAttribute("src", data);
-            console.log(data);
-            downloadLink.setAttribute("href", data);
-            downloadLink.setAttribute("download", "captured_photo.png");    
-            // console.log(capturedPhoto);
-        } else {
-            clearCanvas();
+            video.setAttribute('width', photo_width);
+            video.setAttribute('height', photo_height);
+            canvas.setAttribute('width', photo_width);
+            canvas.setAttribute('height', photo_height);
+            streaming = true;
         }
-    }
+    }, false);
 
-    //adding function to capture button:
-    captureButton.addEventListener(
-        "click",
-        (event) => {
-            takePicture();
-            event.preventDefault();
-        },
-        false,
-    );
+    captureButton.addEventListener('click', (event) => {
+        takePicture();
+        event.preventDefault();
+    }, false);
 
     clearCanvas();
 }
-window.addEventListener("load", init, false)
+
+function clearCanvas() {
+    const context = canvas.getContext('2d');
+    context.fillStyle = '#AAA';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function takePicture() {
+    const context = canvas.getContext('2d');
+    if (photo_width && photo_height) {
+        canvas.width = photo_width;
+        canvas.height = photo_height;
+        context.drawImage(video, 0, 0, photo_width, photo_height);
+
+        const data = canvas.toDataURL('image/png');
+        capturedPhoto.setAttribute('src', data);
+        downloadLink.setAttribute('href', data);
+        downloadLink.setAttribute('download', 'captured_photo.png');
+    } else {
+        clearCanvas();
+    }
+}
+
+window.addEventListener('load', init, false);
